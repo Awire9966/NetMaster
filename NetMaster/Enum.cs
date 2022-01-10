c2f035c2d9ed95cb0ebca7b1b6b7603d9f56e6bf
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2022-01-09 21:57:55 -0500

    Add files via upload

---
+++ NetMaster/Enum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMaster
+{
+    public enum CompilerCodeType
+    {
+      Csharp = 1,
+     VBScript = 2
+    }
+    public enum CodeExceptionHandler
+    {
+        Console = 1,
+        Exception = 2,
+        MessageBox = 3,
+        
+    }
+    public enum ExecutableType
+    {
+        ExeFile = 1,
+        DLLFile = 2
+        
+    }
+}
---
+++ NetMaster/Main.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.CSharp;
+using Microsoft.VisualBasic;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NetMaster
+{
+    public class NetCompiler
+    {
+        public void compile(string code, string reffolder, string outputpath, string outputname, bool runexecutable, CompilerCodeType compilerCodeType, CodeExceptionHandler typeofexception, ExecutableType type)
+        {
+            if (compilerCodeType == CompilerCodeType.Csharp)
+            {
+                CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+                ICodeCompiler icc = codeProvider.CreateCompiler();
+                string Output = outputpath + @"\" + outputname;
+
+               
+
+                System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+                
+                if (type == ExecutableType.ExeFile)
+                {
+                    parameters.GenerateExecutable = true;
+                }
+                if (type == ExecutableType.DLLFile)
+                {
+                    parameters.GenerateExecutable = false;
+                }
+                parameters.OutputAssembly = Output;
+
+                string[] fileEntries = Directory.GetFiles(reffolder);
+                foreach (string fileName in fileEntries)
+                {
+                    parameters.ReferencedAssemblies.Add(fileName);
+                }
+                CompilerResults results = icc.CompileAssemblyFromSource(parameters, code);
+
+                if (results.Errors.Count > 0)
+                {
+
+                    foreach (CompilerError CompErr in results.Errors)
+                    {
+                        if (typeofexception == CodeExceptionHandler.Console)
+                        {
+                            Console.WriteLine(CompErr.ErrorText);
+                        }
+                        if (typeofexception == CodeExceptionHandler.Exception)
+                        {
+                            throw new Exception(CompErr.ErrorText);
+                        }
+                        if (typeofexception == CodeExceptionHandler.MessageBox)
+                        {
+                            MessageBox.Show(CompErr.ErrorText, "Errors in your code: ", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                          
+                        }
+                     
+
+                    }
+                }
+                else
+                {
+
+                    return;
+                }
+                if (compilerCodeType == CompilerCodeType.VBScript)
+                {
+                    VBCodeProvider vbcodeProvider = new VBCodeProvider();
+                    ICodeCompiler vbicc = vbcodeProvider.CreateCompiler();
+                    string vbOutput = outputpath + @"\" + outputname;
+
+
+
+                    System.CodeDom.Compiler.CompilerParameters vbparameters = new CompilerParameters();
+
+                    if (type == ExecutableType.ExeFile)
+                    {
+                        vbparameters.GenerateExecutable = true;
+                    }
+                    if (type == ExecutableType.DLLFile)
+                    {
+                        vbparameters.GenerateExecutable = false;
+                    }
+                    parameters.OutputAssembly = Output;
+
+                    string[] vbfileEntriess = Directory.GetFiles(reffolder);
+                    foreach (string fileName in vbfileEntriess)
+                    {
+                        parameters.ReferencedAssemblies.Add(fileName);
+                    }
+                    CompilerResults vbresults = icc.CompileAssemblyFromSource(parameters, code);
+
+                    if (vbresults.Errors.Count > 0)
+                    {
+
+                        foreach (CompilerError CompErr in vbresults.Errors)
+                        {
+                            if (typeofexception == CodeExceptionHandler.Console)
+                            {
+                                Console.WriteLine(CompErr.ErrorText);
+                            }
+                            if (typeofexception == CodeExceptionHandler.Exception)
+                            {
+                                throw new Exception(CompErr.ErrorText);
+                            }
+                            if (typeofexception == CodeExceptionHandler.MessageBox)
+                            {
+                                MessageBox.Show(CompErr.ErrorText, "Errors in your code: ", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+                            }
+
+
+                        }
+                    }
+                    else
+                    {
+
+                        return;
+                    }
+                }
+            }
+        }
+    
+    }
+
+
+}
